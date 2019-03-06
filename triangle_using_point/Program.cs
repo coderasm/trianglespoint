96f10c6ed3af8869ca412c842795ef3a6398725a
pkrueger <philip.krueger@caci.com>
2019-03-06 15:24:56 -0800

    added non triangle

--- triangle_using_point/Program.cs
+++ triangle_using_point/Program.cs
@@ -7,6 +7,8 @@ namespace triangle_using_point
   {
     static void Main(string[] args)
     {
+      int[][] nonTriangleVertices = new int[3][] { new int[] { 0, 0 }, new int[] { 0, 1 }, new int[] { 0, 1 } };
+      var nonTriangle = new Triangle(nonTriangleVertices);
       int[][] rightTriangleVertices = new int[3][] { new int[]{ 0, 0 }, new int[]{ 0, 1 }, new int[]{ 1, 0 } };
       var rightTriangle = new Triangle(rightTriangleVertices);
       int[][] obtuseTriangleVertices = new int[3][] { new int[] { 0, 0 }, new int[] { -1, 1 }, new int[] { 1, 0 } };
@@ -14,6 +16,7 @@ namespace triangle_using_point
       int[][] acuteTriangleVertices = new int[3][] { new int[] { 0, 0 }, new int[] { 1, 3 }, new int[] { 3, 0 } };
       var acuteTriangle = new Triangle(acuteTriangleVertices);
 
+      nonTriangle.TestIfTriangle();
       rightTriangle.TestIfTriangle();
       obtuseTriangle.TestIfTriangle();
       acuteTriangle.TestIfTriangle();
